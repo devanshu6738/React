4056764855e33e6bcc4aeefe7b7afd51803f1e53
Devanshu Gupta <devanshu9167@gmail.com>
2025-01-07 11:11:30 +0530

    react project1 completed

--- React Project 1/src/App.jsx
+++ React Project 1/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import Allcards from './component/Allcard'
+import {data} from './utilis/data'
 
 function App() {
+  const[val,Setval]=useState('')
+  const[filterData,SetfilterData]=useState(data)
+  function filterHandler(){
+    let res=data.filter((item)=>item.title.toLowerCase().includes(val.toLowerCase()))
+    SetfilterData(res);
+    
+  }
   return (
     <div>
       <h1>E-commerce Website</h1>
       <div>
-        <input type="text" placeholder='Search Products..' />
-        <button>Search</button>
+        <input type="text" placeholder='Search Products..' onChange={(e)=> Setval(e.target.value)} />
+        <button onClick={filterHandler}>Search</button>
       </div>
-      <Allcards/>
+      <Allcards data={filterData}/>
     </div>
   )
 }
