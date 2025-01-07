5c1f0eb331dfa21ab5270b6c8b2eedbedeb971e7
Devanshu Gupta <devanshu9167@gmail.com>
2025-01-07 13:24:29 +0530

    giving a tailwind

--- React Project 1/src/App.jsx
+++ React Project 1/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
     
   }
   return (
-    <div>
-      <h1>E-commerce Website</h1>
-      <div>
-        <input type="text" placeholder='Search Products..' onChange={(e)=> Setval(e.target.value)} />
-        <button onClick={filterHandler}>Search</button>
+     <div className='bg-[#0B1C2E] text-white p-10 min-h-screen  '>
+      <h1 className='text-5xl font-bold'>E-commerce Website</h1>
+      <div className='flex gap-2 mt-5'>
+        <input type="text" placeholder='Search Products..' onChange={(e)=> Setval(e.target.value)} className='w-96 h-12 text-black outline-none rounded-md p-2'/>
+        <button onClick={filterHandler} className='pl-5 pr-5 w-28 bg-white rounded-md text-black'>Search</button>
       </div>
       <Allcards data={filterData}/>
     </div>
--- React Project 1/src/component/Allcard.jsx
+++ React Project 1/src/component/Allcard.jsx
@@ -1,20 +1,13 @@
 function Allcards({data}){
   return(
-    <div style={{display:"flex",flexWrap:"wrap",gap:"20px",marginTop:"20px",justifyContent:"center"}}>
+    <div className="flex flex-wrap gap-20 justify-center mt-8">
       {data.map(singleItem =>(
-        <div
-        key={singleItem.id}
-         style={{
-          backgroundColor:"black",
-          width:"350px",
-          height:"450px",
-          color:"white",
-          padding:"20px"
-        }}>
+        <div className="w-[350px] min-h-[420px] bg-[#193351] p-4 rounded-lg"
+        key={singleItem.id}>
           <img src={singleItem.thumbnail} alt="" />
-          <p>{singleItem.title}</p>
-          <p>{singleItem.price}$</p>
-          <p>{singleItem.description}</p>
+          <p className="text-xl font-semibold">{singleItem.title}</p>
+          <p className="text-[#CDCDCD]">{singleItem.price}$</p>
+          <p className="w-300px">{singleItem.description}</p>
         </div>
       ))}
       
