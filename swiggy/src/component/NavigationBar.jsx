a7294e9d9cf5b70ae4bb61789c97a3de80b49e2c
Devanshu Gupta <devanshu9167@gmail.com>
2025-02-11 15:17:27 +0530

    #

--- swiggy/src/component/NavigationBar.jsx
+++ swiggy/src/component/NavigationBar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 function NavigationBar() {
+  const navItem=[
+    {
+      image:<i className="fi fi-rr-shopping-bag mt-1"></i>,
+      name:"Swiggy Corporate"
+    },
+    {
+      image:<i className="fi fi-rr-search mt-1"></i>,
+      name:"Search"
+    },
+    {
+      image:<i className="fi fi-rr-badge-percent mt-1"></i>,
+      name:"Offers"
+    },
+    {
+      image:<i className="fi fi-rs-interrogation mt-1"></i>,
+      name:"Help"
+    },
+    {
+      image:<i className="fi fi-rs-user mt-1"></i>,
+      name:"Sign Ine"
+    },
+    {
+      image:<i className="fi fi-br-shopping-cart mt-1"></i>,
+      name:"Cart"
+    }
+  ]
   return (
-    <div className='w-full shadow-md h-20 flex justify-center items-center'>
+    <div className='w-full shadow-md h-20 flex justify-center items-center '>
       <div className='flex w-[82%] justify-between'>
       <div className=' flex items-center gap-7'>
         <img src="https://static.vecteezy.com/system/resources/previews/050/816/833/non_2x/swiggy-transparent-icon-free-png.png" alt="" className='w-20'/>
@@ -10,30 +36,14 @@ function NavigationBar() {
         <i className="fi mt-1 fi-rr-angle-small-down text-[#FF5200] text-xl"></i>
       </div>
       <div className='flex items-center gap-12 '>
-        <div className='flex gap-2'>
-        <i className="fi fi-rr-shopping-bag mt-1"></i>
-          <p className='font-semibold'>Swiggy Corporate</p>
-        </div>
-        <div className='flex gap-2'>
-        <i className="fi fi-rr-search mt-1"></i>
-          <p className='font-semibold'>Search</p>
-        </div>
-        <div className='flex gap-2'>
-        <i className="fi fi-rr-badge-percent mt-1"></i>
-          <p className='font-semibold'>Offers</p>
-        </div>
-        <div className='flex gap-2'>
-        <i className="fi fi-rs-interrogation mt-1"></i>
-          <p className='font-semibold'>Help</p>
-        </div>
-        <div className='flex gap-2'>
-        <i className="fi fi-rs-user mt-1"></i>
-          <p className='font-semibold'>Sign In</p>
-        </div>
-        <div className='flex gap-2'>
-        <i className="fi fi-br-shopping-cart mt-1"></i>
-          <p className='font-semibold'>Cart</p>
-        </div>
+        {
+          navItem.map((data)=>(
+            <div className='flex gap-2'>
+            {data.image}
+            <p className='font-semibold text-gray-600'>{data.name}</p>
+          </div>
+          ))
+        }
       </div>
 
       </div>
--- swiggy/src/component/TopRes.jsx
+++ swiggy/src/component/TopRes.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function TopRes() {
+  const [value,Setvalue]=useState(0);
+  const [data,Setdata]=useState([])
+  function handlePrev(){
+    
+    value==0?"":Setvalue((next)=>next+20)
+    
+  }
+  function handleNext(){
+    value==-400?"":Setvalue((prev)=>prev-20)
+  }
+  async function FetchData(){
+        const data=await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=29.15010&lng=75.71760&carousel=true&third_party_vendor=1")
+        const result=await data.json();
+        console.log(result);
+        
+        Setdata(result.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+        
+        // Setdata(result.data.cards[0].card.card.imageGridCards.info)
+      }
+      useEffect(()=>{
+        FetchData();
+      },[])
   return (
-    <div>
-  
+    <div className='w-full mt-8 mb-12'>
+    <div className='w-[78%] mx-auto overflow-x-hidden border-b-1 pb-12 border-gray-200 justify-center'>
+      <div className='flex justify-between mt-4'>
+      <h1 className='text-2xl font-bold '>Top restaurant chains in Delhi</h1>
+      <div className='flex gap-3'>
+      <div className=' bg-[#D8D9DA] rounded-full w-9 h-9 flex items-center justify-center cursor-pointer' onClick={handlePrev}>
+      <i className="fi fi-rr-arrow-small-left text-2xl"></i>
+      </div>
+      <div className=' bg-[#D8D9DA] rounded-full w-9 h-9 flex items-center justify-center cursor-pointer' onClick={handleNext}>
+      <i className="fi fi-rr-arrow-small-right text-2xl"></i>
+      </div>
+      </div>
+      </div>
+    <div className='flex mt-4 gap-6 duration-300' style={{translate:`${value}%`}}>
+      {
+        data.map((items)=>(
+         <div>
+           <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${items.info.cloudinaryImageId}`} alt="" className='min-w-[273px] h-[183px] rounded-2xl'/>  
+         </div>
+        ))
+      }
+      {
+        console.log(value)
+      }
     </div>
+    
+    </div>
+  </div>
   )
 }
 
